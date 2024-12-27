f2b31e6932b91885b5453759fbe2ecdef4ce9178
Gregory Kramar <archiebit8@gmail.com>
2024-12-27 17:30:56 +0400

    Started function processing.

---
+++ src/moogl.exe/function.cc
@@ -0,0 +1,31 @@
+#include <moo/function.hh>
+#include <moo/input.hh>
+
+#include <iostream>
+
+
+namespace moo
+{
+    std::map<std::string, function> functions;
+
+
+    void function::append_list( pugi::xml_node list )
+    {
+
+    }
+
+    void function::append_node( pugi::xml_node node )
+    {
+
+    }
+
+    void function::remove_list( pugi::xml_node list )
+    {
+
+    }
+
+    void function::remove_node( pugi::xml_node node )
+    {
+
+    }
+}
